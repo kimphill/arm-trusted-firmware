0f75da8cfa83e0e119cc1eb170c2606460bf0afe
Kim Phillips <kim.phillips@arm.com>
2017-06-20 11:33:13 -0500

    make make work on kimphi01 not kim

--- make.sh
+++ make.sh
@@ -1,6 +1,6 @@
 #CROSS_COMPILE=aarch64-none-elf- make PLAT=fvp all
 PATH=$HOME/toolchains/gcc-linaro-5.3.1-2016.05-x86_64_aarch64-linux-gnu/bin:$PATH
-BL33=/home/kim/model-work/fvp-latest-oe-uboot/fip-surgery/nt-fw.bin CROSS_COMPILE=aarch64-linux-gnu- make PLAT=fvp all fip
+BL33=/home/kimphi01/model-work/fvp-latest-oe-uboot/fip-surgery/nt-fw.bin CROSS_COMPILE=aarch64-linux-gnu- make PLAT=fvp all fip
 
 
 
