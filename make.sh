1e072f5b6dd660cb0b7729518954c9bd7fc9b7d6
Kim Phillips <kim.phillips@arm.com>
2017-07-18 18:56:47 -0500

    need to set major and minor arch versions to get spe to kick in

--- make.sh
+++ make.sh
@@ -3,6 +3,6 @@
 #BL33=/home/$USER/model-work/fvp-latest-oe-uboot/fip-surgery/nt-fw.bin CROSS_COMPILE=aarch64-linux-gnu- make PLAT=fvp all fip
 make ARCH=aarch64 CROSS_COMPILE=aarch64-linux-gnu- PLAT=fvp distclean
 #make V=1 BL33=/home/$USER/model-work/fvp-latest-oe-uboot/fip-surgery/nt-fw.bin ARCH=aarch64 CROSS_COMPILE=aarch64-linux-gnu- PLAT=fvp all fip
-make V=1 BL33=/home/$USER/git/u-boot/aemv8/u-boot.bin ARCH=aarch64 CROSS_COMPILE=aarch64-linux-gnu- PLAT=fvp all fip
+make V=1 BL33=/home/$USER/git/u-boot/aemv8/u-boot.bin ARCH=aarch64 ARM_ARCH_MAJOR=8 ARM_ARCH_MINOR=2 CROSS_COMPILE=aarch64-linux-gnu- PLAT=fvp all fip
 if [ ! -f build/fvp/release/bl1.bin ]; then echo that didnt work for bl1.bin; fi
 if [ ! -f build/fvp/release/fip.bin ]; then echo that didnt work for fip.bin; fi
