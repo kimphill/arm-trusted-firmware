10e6a9bb0be8258a5752b804ddcac12c9aebcff0
Kim Phillips <kim.phillips@arm.com>
2017-07-18 17:50:22 -0500

    yikes, this and u-boot update got us from bad ramdisk (overlapped images?) to starting kerenl (need kernel console patches, right?!)

--- make.sh
+++ make.sh
@@ -1,5 +1,5 @@
 #CROSS_COMPILE=aarch64-none-elf- make PLAT=fvp all
-export PATH=$HOME/toolchains/gcc-linaro-5.3.1-2016.05-x86_64_aarch64-linux-gnu/bin:$PATH
+#export PATH=$HOME/toolchains/gcc-linaro-5.3.1-2016.05-x86_64_aarch64-linux-gnu/bin:$PATH
 #BL33=/home/$USER/model-work/fvp-latest-oe-uboot/fip-surgery/nt-fw.bin CROSS_COMPILE=aarch64-linux-gnu- make PLAT=fvp all fip
 make ARCH=aarch64 CROSS_COMPILE=aarch64-linux-gnu- PLAT=fvp distclean
 #make V=1 BL33=/home/$USER/model-work/fvp-latest-oe-uboot/fip-surgery/nt-fw.bin ARCH=aarch64 CROSS_COMPILE=aarch64-linux-gnu- PLAT=fvp all fip
